519874f8a5718f894106a6e1e544673a343dd5d3
root <root@localhost.localdomain>
2013-10-26 17:37:50 +0900

    1

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,7 @@
+class ClientsController < ApplicationController
+  def index
+    url = "http://172.16.1.205:8080/axis2/services/Version?wsdl" 
+    @driver = SOAP::WSDLDriverFactory.new(url).create_rpc_driver
+    render :text => @driver.getVersion({}).m_return
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoapClient::Application.config.secret_token = '7c6bcbb9ae5a23d9d87f82c76d26c2644dff7214ab4a70956c036eca8a8d80e81e145d41eae8d4b11f4df7fb0c03bf7e3235c6ab7ab3aec424c031a2c46a2490'
