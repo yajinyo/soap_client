a47f4abc84bf6c1b90aa2566e08ef9910dd495b6
yajinyo <gon05_yadora@olive.plala.or.jp>
2013-11-18 14:59:04 +0900

    Update clients_controller.rb

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,6 +2,13 @@ class ClientsController < ApplicationController
   def index
     url = "http://172.16.1.205:8080/axis2/services/UserManagementService?wsdl" 
     @driver = SOAP::WSDLDriverFactory.new(url).create_rpc_driver
-    render :text => @driver.getVersion({:a=> {:b=> {:c=> "1"}     }      }).m_return
+    
+    
+    ret = @driver.getVersion({:address=> {:zipcode=> "6900011"}  }).m_return
+    
+    
+    render :text => ret.address_list[0].prefecture.name
+    
+#    render :text => @driver.getVersion({:a=> {:b=> {:c=> "1"}     }      }).m_return
   end
 end
